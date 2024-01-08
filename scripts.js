e7b4e2cde59f0228e72bf08790a9582399adc2e5
Kevin <epickillerdude2007@gmail.com>
2024-01-08 08:10:54 -0500

    Added mouseover

--- scripts.js
+++ scripts.js
@@ -3,6 +3,16 @@ const container = document.querySelector('div')
 
 for(let i = 0; i < 256; i++) {
     const square = document.createElement('div')
+    square.addEventListener("mouseover", mouseover)
     square.classList.add('square')
     container.appendChild(square)
+    
+    function mouseover() {
+        square.style.backgroundColor = "yellow"
+    
+    }
 }
+
+
+
+
