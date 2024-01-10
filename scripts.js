2888bd479d91e3b68380a160c1e98c634bfb101b
Kevin <epickillerdude2007@gmail.com>
2024-01-09 21:33:24 -0500

    Changing size finished

--- scripts.js
+++ scripts.js
@@ -1,17 +1,39 @@
-const container = document.querySelector('div')
+
+let container = document.querySelector('#container')
 
 
-for(let i = 0; i < 256; i++) {
-    const square = document.createElement('div')
-    square.addEventListener("mouseover", mouseover)
-    square.classList.add('square')
-    container.appendChild(square)
+function size() {
     
-    function mouseover() {
-        square.style.backgroundColor = "yellow"
-    
+    for(let i = 0; i < inputVal * inputVal; i++) {
+        let square = document.createElement('div')
+        square.addEventListener("mouseover", mouseover)
+        square.classList.add('square')
+        container.appendChild(square)
+
+        function mouseover() {
+            square.style.backgroundColor = "red"
+        }
+        
     }
 }
+
+const button = document.querySelector('button')
+button.addEventListener("click", buttonClick)
+let inputVal = prompt("How many numbers")
+
+
+
+function buttonClick(inputVal) {
+    container.style.gridTemplateRows = `repeat(${inputVal} , 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${inputVal} , 1fr)`;
+}
+size()
+buttonClick(inputVal)
+
+
+
+
+
 
 
 
